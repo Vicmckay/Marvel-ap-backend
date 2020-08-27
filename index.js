dcde76d1b82ec2b1ee4eaf22c5c3cfbbeb7af4d4
Eric Bouchegit config --global user.name Eric <ericbouche@MacBook-Pro-de-Eric.local>
2020-08-27 16:46:59 +0200

    routes

--- index.js
+++ index.js
@@ -1,105 +1,23 @@
 const express = require("express");
 const formidableMiddleware = require("express-formidable");
-const axios = require("axios");
-const cors = require("cors");
+var cors = require("cors");
+
 const app = express();
 require("dotenv").config();
 
 app.use(formidableMiddleware());
 app.use(cors());
 
-// Mes données APIKey :
-const public_Key = process.env.MARVEL_PUBLIC_KEY;
-const private_Key = process.env.MARVEL_SECRET_KEY;
+const marvelRoutes = require("./routes/marvel");
 
-// Générer un Hash :
-const md5 = require("md5");
-const uid2 = require("uid2");
-const ts = uid2(8);
-const hash = md5(ts + private_Key + public_Key);
+app.use(marvelRoutes);
 
-// accès à la base Marvel
-app.get("/characters", async (req, res) => {
-  const offset = req.query.offset;
-  try {
-    const response = await axios.get(
-      "http://gateway.marvel.com/v1/public/characters",
-      {
-        params: {
-          apikey: public_Key,
-          ts: ts,
-          hash: hash,
-          offset: offset,
-          limit: 20,
-        },
-        Headers: {
-          Accept: "*/*",
-        },
-      }
-    );
-    return res.json(response.data);
-  } catch (error) {
-    return res.status(400).json({ message: error.message });
-  }
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome on Marvel API" });
 });
 
-// accès à la base Comics
-app.get("/comics", async (req, res) => {
-  const offset = req.query.offset;
-
-  try {
-    const response = await axios.get(
-      "http://gateway.marvel.com/v1/public/comics",
-      {
-        params: {
-          apikey: public_Key,
-          ts: ts,
-          hash: hash,
-          offset: offset,
-        },
-        Headers: {
-          Accept: "*/*",
-        },
-      }
-    );
-    return res.json(response.data);
-  } catch (error) {
-    return res.status(400).json({ message: error.message });
-  }
+app.all("*", (req, res) => {
+  res.json({ message: "this page does not exist" });
 });
 
-app.get("/comics/:characterId", async (req, res) => {
-  const offset = req.query.offset;
-  let characterId = req.params.characterId;
-
-  try {
-    const response = await axios.get(
-      `http://gateway.marvel.com/v1/public/characters/${characterId}/comics`,
-      {
-        params: {
-          apikey: public_Key,
-          ts: ts,
-          hash: hash,
-          offset: offset,
-          characterId: characterId,
-        },
-        Headers: {
-          Accept: "*/*",
-        },
-      }
-    );
-    console.log(response.data);
-    return res.json(response.data);
-  } catch (error) {
-    return res.status(400).json({ message: error.message });
-  }
-});
-
-// Ma route pour intercepter les routes qui n'existent pas :
-// app.all("*", function (req, res) {
-//  res.json({ message: "Page not found" });
-// });
-
-app.listen(process.env.PORT, () => {
-  console.log("Server started");
-});
+app.listen(process.env.PORT, () => console.log("Server started"));
